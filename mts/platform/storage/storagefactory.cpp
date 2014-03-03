sailfishos/buteo-mtp

commit e578f5a5a50bfa696a1925d28812cb61199db354

[propertycache] Move handling of ObjectPropertyCache to StorageFactory
* Up to now cache had to be updated after each manipulation with object
  handles using StorageFactory API. The operations on the cache are now
  encapsulated into StorageFactory and thus transparent to its users.
* Lessens complexity of MTPResponder, moving the responsibility for the
  cache to a more simple class.

mts/platform/storage/storagefactory.cpp
@@ -30,8 +30,11 @@
 */
 
 #include <dlfcn.h>
-#include "storagefactory.h"
+
 #include <QDir>
+
+#include "objectpropertycache.h"
+#include "storagefactory.h"
 #include "storageplugin.h"
 #include "mtpevent.h"
 #include "trace.h"
@@ -41,7 +44,9 @@ using namespace meegomtp1dot0;
 /*******************************************************
  * StorageFactory::StorageFactory
  ******************************************************/
-StorageFactory::StorageFactory() :  m_storageId(0), m_storagePluginsPath( pluginLocation ), m_newObjectHandle(0), m_newPuoid(0)
+StorageFactory::StorageFactory(): m_storageId(0),
+        m_storagePluginsPath(pluginLocation), m_newObjectHandle(0),
+        m_newPuoid(0), m_objectPropertyCache(*ObjectPropertyCache::instance())
 {
     //TODO For now handle only the file system storage plug-in. As we have more storages
     // make this generic.
@@ -284,6 +289,9 @@ MTPResponseCode StorageFactory::deleteItem( const ObjHandle& handle, const MTPOb
             }
         }
     }
+
+    m_objectPropertyCache.remove(handle);
+
     return response;
 }
 
@@ -429,8 +437,15 @@ MTPResponseCode StorageFactory::moveObject( const ObjHandle &handle, const ObjHa
 
     StoragePlugin *storage = storageOfHandle(handle);
     if (storage) {
-        return storage->moveObject(handle, parentHandle,
+        MTPResponseCode response = storage->moveObject(handle, parentHandle,
                 m_allStorages[destinationStorageId]);
+        if (response == MTP_RESP_OK) {
+            // Invalidate the parent handle property in the cache. The other
+            // properties do not change.
+            m_objectPropertyCache.remove(handle, MTP_OBJ_PROP_Parent_Obj);
+        }
+
+        return response;
     }
 
     return MTP_RESP_InvalidObjectHandle;
@@ -505,9 +520,29 @@ MTPResponseCode StorageFactory::getObjectPropertyValue( const ObjHandle &handle,
                                                         QList<MTPObjPropDescVal> &propValList,
                                                         bool getFromObjInfo, bool getDynamically )
 {
+    QList<MTPObjPropDescVal> notFoundList;
+
+    if (propValList.count() == 1) {
+        if (m_objectPropertyCache.get(handle, propValList[0])) {
+            return MTP_RESP_OK;
+        }
+        notFoundList.swap(propValList);
+    } else {
+        m_objectPropertyCache.get(handle, propValList, notFoundList);
+        if (notFoundList.isEmpty()) {
+            return MTP_RESP_OK;
+        }
+    }
+
     StoragePlugin *storage = storageOfHandle(handle);
     if (storage) {
-        return storage->getObjectPropertyValue(handle, propValList, getFromObjInfo, getDynamically);
+        MTPResponseCode response =
+                storage->getObjectPropertyValue(handle, notFoundList);
+        if (response == MTP_RESP_OK) {
+            m_objectPropertyCache.add(handle, notFoundList);
+            propValList += notFoundList;
+        }
+        return response;
     }
 
     return MTP_RESP_InvalidObjectHandle;
@@ -519,7 +554,11 @@ MTPResponseCode StorageFactory::setObjectPropertyValue( const ObjHandle &handle,
 {
     StoragePlugin *storage = storageOfHandle(handle);
     if (storage) {
-        return storage->setObjectPropertyValue(handle, propValList, sendObjectPropList);
+        MTPResponseCode response =
+                storage->setObjectPropertyValue(handle, propValList, sendObjectPropList);
+        if (response == MTP_RESP_OK) {
+            m_objectPropertyCache.add(handle, propValList);
+        }
     }
 
     return MTP_RESP_InvalidObjectHandle;

mts/platform/storage/storagefactory.h
@@ -43,6 +43,8 @@ class QDir;
 namespace meegomtp1dot0
 {
 class StoragePlugin;
+class ObjectPropertyCache;
+
 const QString pluginLocation = "/usr/lib/mtp";
 const QString CREATE_STORAGE_PLUGINS = "createStoragePlugins";
 const QString DESTROY_STORAGE_PLUGIN = "destroyStoragePlugin";
@@ -244,6 +246,8 @@ private:
     ObjHandle m_largestObjectHandle;
     ObjHandle m_newObjectHandle;
     MtpInt128 m_newPuoid;
+
+    ObjectPropertyCache &m_objectPropertyCache;
 };
 }
 

mts/protocol/mtpevent.cpp
@@ -33,6 +33,7 @@
 #include "mtptypes.h"
 #include "mtpresponder.h"
 #include "mtptxcontainer.h"
+#include "objectpropertycache.h"
 #include "trace.h"
 
 using namespace meegomtp1dot0;
@@ -76,9 +77,8 @@ void MTPEvent::dispatchEvent()
     }
     if(MTP_EV_ObjectPropChanged == m_eventCode)
     {
-        // Request the responder to invalidate any cache for this object
-        // property
-        MTPResponder::instance()->invalidatePropertyCache(m_eventParams[0], m_eventParams[1]);
+        // Invalidate cache for this object property.
+        ObjectPropertyCache::instance()->remove(m_eventParams[0], m_eventParams[1]);
     }
     bool sent = MTPResponder::instance()->sendContainer(eventContainer);
     if( !sent )

mts/protocol/mtpresponder.cpp
@@ -90,7 +90,6 @@ MTPResponder::MTPResponder(): m_storageServer(0),
     m_transporter(0),
     m_devInfoProvider(new DeviceInfoProvider),
     m_propertyPod(PropertyPod::instance(m_devInfoProvider, m_extensionManager)),
-    m_propCache(ObjectPropertyCache::instance()),
     m_extensionManager(new MTPExtensionManager),
     m_copiedObjHandle(0),
     m_containerToBeResent(false),
@@ -1248,7 +1247,6 @@ void MTPResponder::deleteObjectReq()
         QVector<quint32> params;
         reqContainer->params(params);
         code = m_storageServer->deleteItem(params[0], static_cast<MTPObjFormatCode>(params[1]));
-        m_propCache->remove( params[0] );
     }
     sendResponse(code);
 }
@@ -1436,12 +1434,6 @@ void MTPResponder::moveObjectReq()
         else
         {
             code = m_storageServer->moveObject(params[0], params[2], params[1]);
-            if( MTP_RESP_OK == code )
-            {
-                // Invalidate the parent handle property from the cache. The
-                // other properties do not change
-                m_propCache->remove( params[0], MTP_OBJ_PROP_Parent_Obj );
-            }
         }
     }
 
@@ -1614,18 +1606,7 @@ void MTPResponder::getObjPropValueReq()
 
             propValList.append(MTPObjPropDescVal(propDesc));
 
-            if( m_propCache->get( handle, propValList[0] ) )
-            {
-                code = MTP_RESP_OK;
-            }
-            else
-            {
-                code = m_storageServer->getObjectPropertyValue(handle, propValList);
-                if( MTP_RESP_OK == code )
-                {
-                    m_propCache->add( handle, propValList[0] );
-                }
-            }
+            code = m_storageServer->getObjectPropertyValue(handle, propValList);
             if(MTP_RESP_ObjectProp_Not_Supported == code)
             {
                 QString path;
@@ -1824,14 +1805,8 @@ void MTPResponder::getObjectPropListReq()
                                 {
                                     propValList.append(MTPObjPropDescVal(propDesc));
                                     QList<MTPObjPropDescVal> currPropValList = propValList.mid(pos);
-                                    MTPResponseCode code = m_storageServer->getObjectPropertyValue(currentObj,
-                                                                                                   currPropValList,
-                                                                                                   true,
-                                                                                                   false);
-                                    if( MTP_RESP_OK == code )
-                                    {
-                                        m_propCache->add( currentObj, currPropValList[0] );
-                                    }
+                                    m_storageServer->getObjectPropertyValue(currentObj,
+                                            currPropValList, true, false);
                                     ++pos;
                                 }
                             }
@@ -2223,8 +2198,7 @@ void MTPResponder::sendObjectData(quint8* data, quint32 dataLen, bool isFirstPac
                 }
             }
             m_storageServer->setObjectPropertyValue(handle, propValList, true);
-            m_propCache->add( handle, propValList );
-            m_propCache->setAllProps( handle );
+            ObjectPropertyCache::instance()->setAllProps(handle);
         }
         // Trigger close file in the storage server... ignore return here
         m_storageServer->writeData( handle, 0, 0, false, true);
@@ -2283,7 +2257,6 @@ void MTPResponder::setObjectPropListData()
                     recvContainer->deserializeVariantByType(datatype, propValList[0].propVal);
                     // Set the object property value
                     respCode = m_storageServer->setObjectPropertyValue(objHandle, propValList);
-                    m_propCache->add( objHandle, propValList );
                 }
             }
         }
@@ -2472,7 +2445,6 @@ void MTPResponder::setObjPropValueData()
                 propValList.append(MTPObjPropDescVal(propDesc));
                 recvContainer->deserializeVariantByType(propDesc->uDataType, propValList[0].propVal);
                 respCode = m_storageServer->setObjectPropertyValue(objHandle, propValList);
-                m_propCache->add( objHandle, propValList[0] );
                 if(MTP_RESP_ObjectProp_Not_Supported == respCode)
                 {
                     QString path;
@@ -2785,44 +2757,9 @@ bool MTPResponder::serializePropListQuantum(ObjHandle currentObj, QList<MTPObjPr
 {
     MTP_FUNC_TRACE();
 
-    QList<MTPObjPropDescVal> notFoundList;
-    bool allProps = !(propValList.count() == 1);
-    // Ignore below return value
-    if( allProps )
-    {
-        if( m_propCache->get( currentObj, propValList, notFoundList ) )
-        {
-            ;//Do Nothing
-        }
-        // Not cached yet
-        else
-        {
-            MTP_LOG_INFO("All properties not cached yet, fetch from tracker");
-            MTPResponseCode code = m_storageServer->getObjectPropertyValue(currentObj, notFoundList, false, true);
-            if( MTP_RESP_OK == code )
-            {
-                m_propCache->add( currentObj, notFoundList );
-                m_propCache->setAllProps( currentObj );
-                propValList += notFoundList;
-            }
-        }
-    }
-    else
-    {
-        if( m_propCache->get( currentObj, propValList[0] ) )
-        {
-            ;//Do Nothing
-        }
-        // Not cached yet
-        else
-        {
-            MTP_LOG_INFO("Property not cached yet, fetch from storage/tracker");
-            MTPResponseCode code = m_storageServer->getObjectPropertyValue(currentObj, propValList);
-            if( MTP_RESP_OK == code )
-            {
-                m_propCache->add( currentObj, propValList[0] );
-            }
-        }
+    m_storageServer->getObjectPropertyValue(currentObj, propValList);
+    if (propValList.count() != 1) {
+        ObjectPropertyCache::instance()->setAllProps(currentObj);
     }
     for(QList<MTPObjPropDescVal>::const_iterator i = propValList.constBegin(); i != propValList.constEnd(); ++i)
     {
@@ -2987,8 +2924,3 @@ void MTPResponder::unregisterMetaTypes()
     QMetaType::unregisterType("QVector<MtpInt128>");
 }
 #endif
-
-void MTPResponder::invalidatePropertyCache(ObjHandle objHandle, MTPObjPropertyCode propCode)
-{
-    m_propCache->remove(objHandle, propCode);
-}

mts/protocol/mtpresponder.h
@@ -81,9 +81,6 @@ class MTPResponder : public QObject
         /// \param container [in] The container to be sent
         bool sendContainer(MTPTxContainer &container, bool isLastPacket = true);
         
-        /// Invalidates the object property cache (one property thereof)
-        void invalidatePropertyCache(ObjHandle objHandle, MTPObjPropertyCode propCode);
-
         /// Initiliazes the transport mechanism over which MTP traffic is exchange.
 	/// Call this method after construction the responder.
 	bool initTransport(TransportType transport);
@@ -148,7 +145,6 @@ class MTPResponder : public QObject
         MTPTransporter*                                 m_transporter;      ///< Pointer to the transport layer
         DeviceInfo*                                     m_devInfoProvider;  ///< Pointer to the device info class
         PropertyPod*                                    m_propertyPod;      ///< Pointer to the MTP properties utility class
-        ObjectPropertyCache*                            m_propCache;
         MTPExtensionManager*                            m_extensionManager; ///< Pointer to the MTP extension manager class
         ObjHandle                                       m_copiedObjHandle;  ///< Stored in case the copied object needs to be deleted due to cancel tx
         bool                                            m_containerToBeResent;

commit fe8e36f088e74bd447d313e1d4238dc9b31d4e24

Fix return value of StorageFactory::setObjectPropertyValue()
Bug introduced in e578f5a5a50bfa696a1925d28812cb61199db354.

mts/platform/storage/storagefactory.cpp
@@ -598,6 +598,7 @@ MTPResponseCode StorageFactory::setObjectPropertyValue( const ObjHandle &handle,
         if (response == MTP_RESP_OK) {
             m_objectPropertyCache.add(handle, propValList);
         }
+        return response;
     }
 
     return MTP_RESP_InvalidObjectHandle;