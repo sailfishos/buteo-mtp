f39e7e60ff36cb9587edbff38e6e95ac10050ce6
Simo Piiroinen <simo.piiroinen@jollamobile.com>
2016-04-07 10:49:52 +0300

    [thumbnailer] Request thumbnails in batches. JB#34779
    If there are multitude of images that are missing a thumbnail, sending
    each file individually to thumbnailer creates a lot of dbus related io
    for the mainloop to process. Additionally each finished thumbnail causes
    object property changes which then needs to be sent over to the host,
    which can cause problems during starup when the host is probing the
    device.
    
    Use timer to delay thumbnail requests so that they can be issued in
    batches.
    
    Use longer delay on startup to make it more likely that the device
    reaches storageReady state and host manages to open mtp session before
    thumbnail related property changes start to trickle in.

--- mts/platform/storage/fsstorageplugin/thumbnailer.cpp
+++ mts/platform/storage/fsstorageplugin/thumbnailer.cpp
@@ -46,11 +46,32 @@ static const QString THUMBNAILER_GENERIC_PATH = "/org/freedesktop/thumbnails/Thu
 static const QString NORMAL_DIR = "/normal";
 static const QString THUMB_DIR = "/.thumbnails";
 
+/* How long to wait on startup before starting to generate
+ * missing thumbnails [ms] */
+#define THUMBNAIL_DELAY_ON_STARTUP 3000
+
+/* How long to wait before generating thumbnails when new
+ * pictures are spotted on the file system [ms] */
+#define THUMBNAIL_DELAY_IN_RUNTIME 1000
+
+/* Maximum number of images to pass in one thumbnail request */
+#define THUMBNAIL_MAX_IMAGES_PER_REQUEST 128
 
 Thumbnailer::Thumbnailer() :
     ThumbnailerProxy(THUMBNAILER_SERVICE, THUMBNAILER_GENERIC_PATH, QDBusConnection::sessionBus()),
     m_scheduler("foreground"), m_flavor("normal")
 {
+    /* Setup interval timer for combining multiple thumbnail
+     * requests into one D-Bus method call. The first batch
+     * of thumbnails that need to be generated will be delayed
+     * a bit to keep fs changes minimal during startup.
+     */
+    m_thumbnailTimer = new QTimer(this);
+    m_thumbnailTimer->setSingleShot(false);
+    QObject::connect(m_thumbnailTimer, &QTimer::timeout,
+                     this, &Thumbnailer::thumbnailDelayTimeout);
+    m_thumbnailTimer->setInterval(THUMBNAIL_DELAY_ON_STARTUP);
+
     QString thumbBaseDir = QDir::homePath() + THUMB_DIR;
 
     m_thumbnailDirs << (thumbBaseDir + NORMAL_DIR);
@@ -67,18 +88,20 @@ Thumbnailer::Thumbnailer() :
 
 void Thumbnailer::slotThumbnailReady(uint handle, const QStringList& uris)
 {
-    QString filePath;
     MTP_LOG_TRACE("Thumbnail ready!!");
-    if(m_requestMap.contains(handle))
+
+    foreach(const QString &uri, uris)
     {
-        foreach(QString uri, uris)
-        {
-            filePath = QUrl(uri).path();
-            MTP_LOG_TRACE("Thumbnail ready for::" << filePath);
-            emit thumbnailReady(filePath);
-            m_uriMap.remove(uri);
-        }
-        m_requestMap.remove(handle);
+        /* Thumbnailer may use signals directed to us only
+         * but could as well use regular broadcasts. Ignore
+         * notifications that we are not interested in. */
+        if(!m_uriAlreadyRequested.contains(uri))
+            continue;
+
+        m_uriAlreadyRequested.remove(uri);
+        QString filePath = QUrl(uri).path();
+        MTP_LOG_TRACE("Thumbnail ready for::" << filePath);
+        emit thumbnailReady(filePath);
     }
 }
 
@@ -89,42 +112,96 @@ void Thumbnailer::slotRequestStarted(uint /*handle*/)
 
 void Thumbnailer::slotRequestFinished(uint handle)
 {
-    // We may get a Finished without getting a Ready? In which case, we remove
-    // the request from our internal map...
-    MTP_LOG_TRACE("Thumbnailer request finished::" << handle);
-    QString iri = m_requestMap.value(handle);
-    m_requestMap.remove(handle);
-    // Remove URI...
-    m_uriMap.remove(iri);
+    // Nothing to do here right now
 }
 
-void Thumbnailer::slotError(uint /*handle*/, const QStringList& uris, int /*errorCode*/, const QString& /*errorMsg*/)
+enum {
+    /* Derived from D-Bus API spec */
+    THUMBNAILER_ERROR_UNSUPPORTED_URI_OR_MIME         = 0,
+    THUMBNAILER_ERROR_SPECIALIZED_THUMBNAILER_MISSING = 1,
+    THUMBNAILER_ERROR_INVALID_DATA_OR_MIME            = 2,
+    THUMBNAILER_ERROR_SOURCE_IS_THUMBNAIL             = 3,
+    THUMBNAILER_ERROR_FAILED_TO_WRITE                 = 4,
+    THUMBNAILER_ERROR_UNSUPPORTED_FLAVOR              = 5,
+};
+
+void Thumbnailer::slotError(uint handle, const QStringList& uris, int errorCode, const QString& errorMsg)
 {
+    Q_UNUSED(handle);
+    Q_UNUSED(uris);
+
     // We should always receive a "Finished", even if there is an Error, so we
     // need not clean our maps here.
-    foreach(QString uri, uris)
-    {
-        MTP_LOG_WARNING("Thumbnailer returned error for:: " << uri);
+
+    switch(errorCode) {
+    case THUMBNAILER_ERROR_SOURCE_IS_THUMBNAIL:
+        // Be silent about failures to thumbnail thumbnails
+        break;
+
+    case THUMBNAILER_ERROR_INVALID_DATA_OR_MIME:
+        // Be silent about missing image loaders
+        break;
+
+    default:
+        MTP_LOG_WARNING("Thumbnailer returned error code:" << errorCode
+                        << "message: " << errorMsg);
+        /* At least the currently used thumbnailer reports just one
+         * file / error and the errorMsg identifies the source file
+         * too -> listing uris would be just repetitive noise. */
+#if 0
+        foreach(const QString &uri, uris) {
+            MTP_LOG_WARNING("for uri:: " << uri);
+        }
+#endif
+        break;
     }
 }
 
 void Thumbnailer::requestThumbnailFinished(QDBusPendingCallWatcher *pcw)
 {
     QDBusPendingReply<uint> reply = *pcw;
-    const QString fileIri = pcw->property("thumbnailer_file_iri").toString();
 
     if (reply.isError()) {
-        MTP_LOG_WARNING("Failed to queue request to thumbnailer::" << fileIri);
+        MTP_LOG_WARNING("Failed to queue request to thumbnailer");
         MTP_LOG_WARNING("Error::" << reply.error());
-
-        /* The method call failed, so we are not going to see any
-         * D-Bus signals related to this request -> dequeue uri. */
-        m_uriMap.remove(fileIri);
-    } else {
-        /* Insert handle to map */
-        m_requestMap.insert(reply.value(), fileIri);
     }
+
     pcw->deleteLater();
+
+    /* Note: The uris are left in m_uriAlreadyRequested, which
+     * implicitly blocks repeated thumbnailing attempts. */
+}
+
+void Thumbnailer::thumbnailDelayTimeout()
+{
+    if(m_uriRequestQueue.isEmpty()) {
+        MTP_LOG_TRACE("Thumbnail queue is empty; stopping timer");
+        m_thumbnailTimer->stop();
+
+        /* Setup initial delay for the next patch of requests */
+        m_thumbnailTimer->setInterval(THUMBNAIL_DELAY_IN_RUNTIME);
+        return;
+    }
+
+    /* Dequeue image files & mime-types to thumbnail */
+    QStringList uris;
+    QStringList mimes;
+
+    for(int i = 0; i < THUMBNAIL_MAX_IMAGES_PER_REQUEST; ++i) {
+        if(m_uriRequestQueue.isEmpty())
+            break;
+        uris  << m_uriRequestQueue.takeFirst();
+        mimes << m_uriRequestQueue.takeFirst();
+    }
+
+    /* Make an asynchronous thumbnail request via D-Bus */
+    MTP_LOG_TRACE("Requesting" << uris.count() << "thumbnails");
+    QDBusPendingCall pc = this->Queue(uris, mimes, m_flavor, m_scheduler, 0);
+    QDBusPendingCallWatcher *pcw = new QDBusPendingCallWatcher(pc, this);
+    connect(pcw, SIGNAL(finished(QDBusPendingCallWatcher*)), SLOT(requestThumbnailFinished(QDBusPendingCallWatcher*)));
+
+    /* Continue flushing the queue when mainloop gets idle */
+    m_thumbnailTimer->setInterval(0);
 }
 
 QString Thumbnailer::requestThumbnail(const QString &filePath, const QString &mimeType)
@@ -133,21 +210,19 @@ QString Thumbnailer::requestThumbnail(const QString &filePath, const QString &mi
     QString fileIri = IRI_PREFIX + filePath;
     // First check if a thumbnail is already present
     // If yes, then we simply return the thumbnail path
-    if(!checkThumbnailPresent(filePath, thumbPath) && !m_uriMap.contains(fileIri))
+    if(!checkThumbnailPresent(filePath, thumbPath))
     {
-        // Here we initiate a request to the thumbnailer to generate a new thumbnail
-        QStringList uris;
-        QStringList mimes; // Currently empty -- do we need to fill these?
-        uris << fileIri;
-        mimes << mimeType;
-        QDBusPendingCall pc = this->Queue(uris, mimes, m_flavor, m_scheduler, 0);
-        QDBusPendingCallWatcher *pcw = new QDBusPendingCallWatcher(pc, this);
-        pcw->setProperty("thumbnailer_file_iri", fileIri);
-        connect(pcw, &QDBusPendingCallWatcher::finished,
-                this, &Thumbnailer::requestThumbnailFinished);
+        if(!m_uriAlreadyRequested.contains(fileIri)) {
+            /* Add uri to queue, use dummy handle */
+            m_uriAlreadyRequested.insert(fileIri, 0);
+            m_uriRequestQueue.append(fileIri);
+            m_uriRequestQueue.append(mimeType);
 
-        /* Insert uri to map; value is dummy */
-        m_uriMap.insert(fileIri, 0);
+            /* Queue is flushed via timer to combine multiple
+             * images into one thumbnail request. */
+            if(!m_thumbnailTimer->isActive())
+                m_thumbnailTimer->start();
+        }
     }
     return thumbPath;
 }
--- mts/platform/storage/fsstorageplugin/thumbnailer.h
+++ mts/platform/storage/fsstorageplugin/thumbnailer.h
@@ -85,24 +85,29 @@ class Thumbnailer : public ThumbnailerProxy
         void slotRequestFinished(uint);
         ///< This slot handles the Error signal from the DBUS interface
         void slotError(uint, const QStringList&, int, const QString&);
-        ///< Async handling of requestThumbnail() replies
+        ///< This slot handles async replies from thumbnailer
         void requestThumbnailFinished(QDBusPendingCallWatcher *pcw);
+        ///< This slot handlers flushing of thumbnail request queue
+        void thumbnailDelayTimeout();
+
     private:
         ///< Checks if thumbnail for the requested path is already present in
         /// the system thumbnailer's cache.
         bool checkThumbnailPresent(const QString& filePath, QString& thumbPath);
         ///< A list of available thumbnail directories
         QStringList m_thumbnailDirs;
-        ///< Internal map to keep track of thumbnail requests by handle
-        QHash<uint, QString> m_requestMap;
-        ///< Internal map to keep track of thumbnail requests by URI
-        QHash<QString, uint> m_uriMap;
+        ///< Queue of images that are missing thumbnails
+        QStringList m_uriRequestQueue;
+        ///< Internal map to keep track of pending thumbnail requests
+        QHash<QString, uint> m_uriAlreadyRequested;
         ///< The scheduler to use for the system thumbnailer. The class
         /// currently fixes this to "foreground".
         const QString m_scheduler;
         ///< The "flavor" of the thumbnail needed. The class currently fixes
         /// this to "normal".
         const QString m_flavor;
+        ///< Timer for combining multiple image sources to one thumbnail request
+        QTimer *m_thumbnailTimer;
 };
 }
 #endif // THUMBNAILER_H
